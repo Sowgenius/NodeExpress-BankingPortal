b0e0dd3c40bb40491b5d6cdcfe8cc92cdfbf5c9d
SowGenius <korkasow1@gmail.com>
2020-10-07 02:39:49 +0000

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -7,11 +7,50 @@
  app.set('views', path.join(__dirname, 'views'));
  app.set('view engine', 'ejs');
 
+ //Set static to Public file
  app.use(express.static(path.join(__dirname, 'public')));
 
+ const accountData = fs.readFileSync('src/json/accounts.json', {
+   encoding: 'utf8'
+ });
+
+ const accounts = JSON.parse(accountData);
+
+ const userData = fs.readFileSync('src/json/users.json', {
+   encoding: 'utf8'
+ })
+
+ const users = JSON.parse(userData);
+
+ //Routes
  app.get('/', function (req, res) {
    res.render('index', {
-     title: 'Index'
+     title: 'Account Summary',
+     accounts: accounts
+   });
+ })
+
+ app.get('/profile', function (req, res) {
+   res.render('profile', {
+     user: users[0]
+   });
+ })
+
+ app.get('/savings', function (req, res) {
+   res.render('account', {
+     account: accounts.savings
+   });
+ })
+
+ app.get('/checking', function (req, res) {
+   res.render('account', {
+     account: accounts.checking
+   });
+ })
+
+ app.get('/credit', function (req, res) {
+   res.render('account', {
+     account: accounts.credit
    });
  })
 
